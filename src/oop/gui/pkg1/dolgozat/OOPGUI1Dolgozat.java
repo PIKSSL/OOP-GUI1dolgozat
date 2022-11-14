92cf601150e4af163ddba0ca728da394c7502fb8
PIKSSL <purpledog120@gmail.com>
2022-11-14 12:48:43 +0100

    repo

---
+++ src/oop/gui/pkg1/dolgozat/OOPGUI1Dolgozat.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.gui.pkg1.dolgozat;
+
+/**
+ *
+ * @author gyorgy.krisztian
+ */
+public class OOPGUI1Dolgozat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
